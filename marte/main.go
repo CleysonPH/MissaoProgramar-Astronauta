679c645e753320eabb2a5323bdba23cf07dc5bf4
Cleyson Lima <cleysonph@gmail.com>
2022-02-02 14:26:22 -0300

    missão marte resolvida

---
+++ marte/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// a. -5 + 8 * 6
+// b. (55 + 9)% 9
+// c. 20 + -3 * 5/8
+// d. 5 + 15/3 * 2 - 8% 3
+
+func main() {
+	var (
+		a float64 = -5 + 8*6
+		b float64 = (55 + 9) % 9
+		c float64 = 20 + -3*5.0/8.0
+		d float64 = 5 + 15/3*2 - 8%3
+	)
+	fmt.Printf("a = %.2f\n", a)
+	fmt.Printf("b = %.2f\n", b)
+	fmt.Printf("c = %.2f\n", c)
+	fmt.Printf("d = %.2f\n", d)
+}
